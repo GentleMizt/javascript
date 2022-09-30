df73c9e977f442c6b1dbd147e99710246553c1a4
GentleMizt <olamideiyanda18@gmail.com>
2022-09-30 16:21:47 +0100

    1st commit

--- script.js
+++ script.js
@@ -27,16 +27,80 @@
 // let f = alert(t/z);
 // let q = alert(y/z);
 
-// Quadratic Formula Calc2
-let a = Number(prompt("Input the value of a"));
-let b = Number(prompt("Input the value of b"));
-let c = Number(prompt("Input the value of c"));
+// // Quadratic Formula Calc2
+// let a = Number(prompt("Input the value of a"));
+// let b = Number(prompt("Input the value of b"));
+// let c = Number(prompt("Input the value of c"));
 
-let numerator = -b + (b**2 - 4*a*c);
-let denominator = 2 * a;
-let x = numerator/denominator;
-alert(x);
+// let numerator = -b + (b**2 - 4*a*c);
+// let denominator = 2 * a;
+// let x = numerator/denominator;
+// alert(x);
 
-let numerator2 = -b - (b**2 - 4*a*c);
-let y = numerator2/denominator;
-alert(y);
+// let numerator2 = -b - (b**2 - 4*a*c);
+// let y = numerator2/denominator;
+// alert(y);
+
+// // Quadratic Formula Calc3
+// let a = Number(prompt("Input the value of a"));
+// let b = Number(prompt("Input the value of b"));
+// let c = Number(prompt("Input the value of c"));
+
+// let step_1 = b ** 2 - 4 * a * c;
+
+
+
+
+
+
+
+
+
+// PRACTICE!!!!!!!!!
+// preferrable naming CONVENTION: camelCase
+// symbols apart from Dollars and Underscores are not allowed in naming variables
+
+// USING METHODS FOR NUMBERS
+
+let firstNum = 4.5;
+let secondNum = 3.7
+
+// Rounds off the number to the nearest whole number irrespective of the decimal
+console.log(Math.ceil(firstNum));
+
+// Rounds off the number to the nearest whole number irrespective of the decimal
+console.log(Math.floor(3.7));
+
+// RRounds off the number to the nearest whole number respective of the decimal
+console.log(Math.round(firstNum));
+console.log(Math.round(secondNum));
+
+// EVERY METHOD TAKES IN ARGUMENT THAT IT EVALUATES
+// The Math.pow takes in two arguments 1.The number  2. The Index
+// Math.pow(number, index);
+console.log(Math.pow(2,4));
+
+// Math.sqrt takes only one argument
+console.log(Math.sqrt(16));
+
+// Let's generate a random number using Math.random
+// console.log(Math.round(Math.random() * 10));
+
+// let randNum = Math.random() *10
+// let wholeNum = Math.round(randNum);
+// alert(wholeNum)
+
+// STRING METHODS
+let firstName = "Lancer";
+console.log(firstName);
+
+// converts string to lower case
+console.log(firstName.toLowerCase());
+
+// converts strings to Uppercase
+console.log(firstName.toUpperCase());
+
+// Shows the position of a character in a string
+// JS strings start counting from position 0 not 1
+console.log(firstName.charAt(2));
+console.log(firstName.indexOf("n"));
