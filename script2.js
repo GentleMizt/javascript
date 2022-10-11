20e881280d1256e487858612bce5e998aee058a6
GentleMizt <olamideiyanda18@gmail.com>
2022-10-12 00:42:40 +0100

    js7

--- script2.js
+++ script2.js
@@ -1,27 +1,62 @@
 // Object Literal
+ // const blogs = [
+//     {
+//         title: 'Why Mac & Cheese rules', 
+//         likes: 30
+//     },
+//     { 
+//         title: '10 things to make with marmite', 
+//         likes: 50
+//     }
+// ];
+    // console.log(blogs);
 
-    let user = {
-        name: 'crystal',
-        age: 30,
-        email: 'olamideiyanda18@gmail.com',
-        location: 'Nigeria',
-        blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-        login: function(){
-            console.log('the user logged in');
-        },
-        logout: function(){
-            console.log('the user logged out');
-        },
-        logBlogs: function(){
-            // console.log(this.blogs);
-            console.log('this user has written the following blogs');
-            this.blogs.forEach(blog => {
-                console.log(blog);
-            })
-        } 
-    };
+    // let user = {
+    //     name: 'crystal',
+    //     age: 30,
+    //     email: 'olamideiyanda18@gmail.com',
+    //     location: 'Nigeria',
+    //     blogs: [
+    //         {
+    //             title: 'Why Mac & Cheese rules', 
+    //             likes: 30
+    //         },
+    //         { 
+    //             title: '10 things to make with marmite', 
+    //             likes: 50
+    //         }
+    //     ],
+    //     login: function(){
+    //         console.log('the user logged in');
+    //     },
+    //     logout: function(){
+    //         console.log('the user logged out');
+    //     },
+    //     logBlogs: function(){
+    //         // console.log(this.blogs);
+    //         console.log('this user has written the following blogs');
+    //         this.blogs.forEach(blog => {
+    //             console.log(blog.title, blog.likes);
+    //         })
+    //     } 
+    // };
 
-    user.logBlogs();
+    // user.logBlogs();
     
+    // MATH OBJECT
+    console.log(Math);
+    console.log(Math.PI);
+    console.log(Math.E);
+    
+    const area = 7.7;
+    console.log(Math.round(area));
+    console.log(Math.floor(area));
+    console.log(Math.ceil(area));
+    console.log(Math.trunc(area));
 
+    // Generating random numbers
+
+    const random =  Math.random();
+    console.log(random); 
+    console.log(Math.round(random * 100));
     
